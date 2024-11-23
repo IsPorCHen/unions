510b2146601a22b4d34f7f918bffebc6656e6da1
student6 <zdorovo1993@gmail.com>
2024-11-23 13:21:32 +0500

    Добавьте файлы проекта.

---
+++ unions.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+
+using namespace std;
+
+union Info {
+	struct {
+		string phoneNumber;
+		string address;
+	} clinic;
+
+	struct {
+		char maritalStatus[1];
+	} maritalStatus;
+};
+
+struct Employee {
+	string surname;
+	char gender[1];
+	int daysOnSickLeave;
+};
+int main()
+{
+	Employee employeers[] {
+		{"vasilev", 'm', 21},
+		{"yunusov", 'm', 10}
+	};
+
+	if (employeers->daysOnSickLeave > 21) {
+
+	}
+	else {
+
+	}
+}
